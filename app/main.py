2d3f43a5f70f807d1e23e7a58f08165924068df0
Andrey <adreygichan@gmail.com>
2025-08-24 21:04:41 +0300

    adding deploy

--- app/main.py
+++ app/main.py
@@ -29,4 +29,4 @@ app.include_router(vote.router)
 
 @app.get("/")
 def root():
-    return {"message": "successfully deployed from CI/CD pipeline"}
+    return {"message": "successfully deployed from CI/CD pipeline!"}
